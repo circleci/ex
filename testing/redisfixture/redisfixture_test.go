a8a74d0dc926c974717a99aa095d043d11c31634
Pete Woods <pete.woods@circleci.com>
2022-01-14 12:50:27 +0000

    `testing/redisfixture`: Add connection parameter
    - This is a breaking change, but worth doing it before we start to have too many consumers IMO.

--- testing/redisfixture/redisfixture_test.go
+++ testing/redisfixture/redisfixture_test.go
@@ -10,14 +10,22 @@ import (
 
 func TestSetup(t *testing.T) {
 	ctx := testcontext.Background()
-	fix := Setup(ctx, t)
-	assert.Check(t, fix.Ping(ctx).Err())
-	assert.Check(t, fix.DB > 0)
+	fix := Setup(ctx, t, Connection{Addr: "localhost:6379"})
+	t.Run("Check we can ping", func(t *testing.T) {
+		assert.Check(t, fix.Ping(ctx).Err())
+	})
+	t.Run("Check we got a non zero DB", func(t *testing.T) {
+		assert.Check(t, fix.DB > 0)
+	})
 }
 
 func TestSetupAgain(t *testing.T) {
 	ctx := testcontext.Background()
-	fix := Setup(ctx, t)
-	assert.Check(t, fix.Ping(ctx).Err())
-	assert.Check(t, fix.DB > 0)
+	fix := Setup(ctx, t, Connection{Addr: "localhost:6379"})
+	t.Run("Check we can ping", func(t *testing.T) {
+		assert.Check(t, fix.Ping(ctx).Err())
+	})
+	t.Run("Check we got a non zero DB", func(t *testing.T) {
+		assert.Check(t, fix.DB > 0)
+	})
 }
