efb9d356289cc7af306fd302693139f1b1ebece7
Dan Mullineux <danmux@circleci.com>
2022-02-22 09:40:35 +0000

    Remove redundant code
    We now no longer have the combined binary in any service
    so the honeycomb coordinator is no longer needed.
    
    There is already a udp fake statsd server, so use that in
    the httpclient/metrics test.

--- httpclient/metrics/metrics_test.go
+++ httpclient/metrics/metrics_test.go
@@ -1,28 +1,24 @@
 package metrics
 
 import (
-	"bufio"
 	"context"
-	"io"
-	"net"
 	"net/http"
 	"net/http/httptest"
-	"os"
-	"path/filepath"
 	"strings"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/DataDog/datadog-go/statsd"
-	"golang.org/x/sync/errgroup"
 	"gotest.tools/v3/assert"
 	"gotest.tools/v3/assert/cmp"
+	"gotest.tools/v3/poll"
 
 	"github.com/circleci/ex/httpclient"
 	"github.com/circleci/ex/o11y"
 	"github.com/circleci/ex/o11y/honeycomb"
 	"github.com/circleci/ex/system"
+	"github.com/circleci/ex/testing/fakestatsd"
 	"github.com/circleci/ex/testing/testcontext"
 )
 
@@ -113,8 +109,7 @@ func TestMetrics(t *testing.T) {
 		wg.Wait()
 
 		cancel()
-		metrics, err := done() // wait for the stats server to stop
-		assert.NilError(t, err)
+		metrics := done() // wait for the stats to be flushed
 
 		assert.Check(t, len(metrics) > 900, len(metrics))
 		assert.Check(t, len(metrics) < 1000, len(metrics))
@@ -191,8 +186,7 @@ func TestMetrics(t *testing.T) {
 		wg.Wait()
 
 		cancel()
-		metrics, err := done() // wait for the stats server to stop
-		assert.NilError(t, err)
+		metrics := done() // wait for the stats server to stop
 
 		assert.Check(t, len(metrics) > 150, len(metrics))
 		assert.Check(t, len(metrics) < 220, len(metrics))
@@ -204,80 +198,46 @@ func TestMetrics(t *testing.T) {
 	})
 }
 
-/*
-
-   metrics_test.go:107: assertion failed: 968 (int) != 974 (int)
-   metrics_test.go:109: assertion failed: expression is false: count <= max: count:26 > max:20
-   metrics_test.go:110: assertion failed: expression is false: count <= max: count:26 > max:20
-   metrics_test.go:113: assertion failed: expression is false: count >= min: count:84 < min:90
-
-    metrics_test.go:182: assertion failed: expression is false: count <= max: count:2 > max:0
-    metrics_test.go:183: assertion failed: expression is false: count <= max: count:4 > max:0
-    metrics_test.go:185: assertion failed: expression is false: count >= min: count:96 < min:100
-
-*/
-
-func assertIn(t *testing.T, ls []string, what string, min, max int) {
+func assertIn(t *testing.T, ls []fakestatsd.Metric, what string, min, max int) {
 	t.Helper()
 	count := 0
+
 	for _, l := range ls {
-		if strings.Contains(l, what) {
-			count++
+		for _, tag := range l.Tags {
+			if strings.Contains(tag, what) {
+				count++
+			}
 		}
 	}
+	t.Logf("found %d matches", count)
 	assert.Check(t, count >= min, "count:%d < min:%d", count, min)
 	assert.Check(t, count <= max, "count:%d > max:%d", count, max)
 }
 
-func setupMetrics(t *testing.T, ctx context.Context) (context.Context, func() ([]string, error)) {
-	socket := filepath.Join(os.TempDir(), "httpclient-trace-test.sock")
-	t.Cleanup(func() {
-		_ = os.Remove(socket) // ignore errors
-	})
+func setupMetrics(t *testing.T, ctx context.Context) (context.Context, func() []fakestatsd.Metric) {
+	s := fakestatsd.New(t)
 
-	conn, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 0})
-	assert.NilError(t, err)
-	stats, err := statsd.New(conn.LocalAddr().String())
+	stats, err := statsd.New(s.Addr())
 	assert.Assert(t, err)
 
-	g, ctx := errgroup.WithContext(ctx)
 	t.Cleanup(func() {
 		_ = stats.Close() // not bothered about close errors
 	})
-	var metrics []string
-	g.Go(udpServerFn(ctx, conn, &metrics))
 
-	done := func() ([]string, error) {
-		e := g.Wait()
-		return metrics, e
+	done := func() []fakestatsd.Metric {
+		var metrics []fakestatsd.Metric
+		poll.WaitOn(t, func(t poll.LogT) poll.Result {
+			metrics = s.Metrics()
+			if len(metrics) == 0 {
+				return poll.Continue("no metrics found yet")
+			}
+			return poll.Success()
+		})
+		return metrics
 	}
+
 	return o11y.WithProvider(ctx, honeycomb.New(honeycomb.Config{
 		Format:  "color",
 		Metrics: stats,
 	})), done
-}
-
-func udpServerFn(ctx context.Context, r io.ReadCloser, rb *[]string) func() error {
-	return func() error {
-		done := make(chan struct{})
-		go func() {
-			scanner := bufio.NewScanner(r)
-			scanner.Split(bufio.ScanLines)
-			for scanner.Scan() {
-				*rb = append(*rb, scanner.Text())
-			}
-			close(done)
-		}()
-
-		for {
-			select {
-			case <-ctx.Done():
-				_ = r.Close()
-				<-done
-				return nil
-			case <-done:
-				return nil
-			}
-		}
-	}
 }
