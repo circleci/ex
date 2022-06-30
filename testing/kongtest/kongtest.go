521ff30d68cba954fab072a70e47bd665e6ba0c6
Pete Woods <pete.woods@circleci.com>
2022-06-30 09:57:32 +0100

    Fix kong break

--- testing/kongtest/kongtest.go
+++ testing/kongtest/kongtest.go
@@ -5,33 +5,25 @@ import (
 	"testing"
 
 	"github.com/alecthomas/kong"
+	"github.com/google/go-cmp/cmp"
 	"gotest.tools/v3/assert"
-	"gotest.tools/v3/assert/cmp"
 )
 
 func Help(t *testing.T, cli interface{}) string {
 	w := bytes.NewBuffer(nil)
-	exited := false
+	rc := -1
 	app, err := kong.New(cli,
 		kong.Name("test-app"),
 		kong.Writers(w, w),
-		kong.Exit(func(int) {
-			exited = true
-			panic("value for panic") // Panic to fake "exit".
+		kong.Exit(func(i int) {
+			rc = i
 		}),
 	)
 	assert.Check(t, err)
 
-	var value interface{}
-	func() {
-		defer func() {
-			value = recover()
-		}()
-		_, err := app.Parse([]string{"--help"})
-		assert.Check(t, err)
-	}()
-	assert.Check(t, cmp.Equal(value, "value for panic"))
-	assert.Check(t, exited)
+	_, err = app.Parse([]string{"--help"})
+	assert.Check(t, err)
+	assert.Check(t, cmp.Equal(0, rc))
 
 	return w.String()
 }
