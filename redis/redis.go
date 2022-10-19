e25b634859c5b2e20df64f3b8a25f0c437eec62b
Pete Woods <pete.woods@circleci.com>
2022-10-19 09:08:10 +0100

    Revert "Upgrade Redis client to v9"
    This reverts commit cf2da3a2cd7d7908b18dff68149db97522f2d998.

--- redis/redis.go
+++ redis/redis.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/go-redis/redis/v9"
+	"github.com/go-redis/redis/v8"
 
 	"github.com/circleci/ex/config/secret"
 )
@@ -62,22 +62,25 @@ type Options struct {
 	// Maximum number of socket connections.
 	// Default is 10 connections per every available CPU as reported by runtime.GOMAXPROCS.
 	PoolSize int
+	// Minimum number of idle connections which is useful when establishing
+	// new connection is slow.
+	MinIdleConns int
+	// Connection age at which client retires (closes) the connection.
+	// Default is to not close aged connections.
+	MaxConnAge time.Duration
 	// Amount of time client waits for connection if all connections
 	// are busy before returning an error.
 	// Default is ReadTimeout + 1 second.
 	PoolTimeout time.Duration
-	// Minimum number of idle connections which is useful when establishing
-	// new connection is slow.
-	MinIdleConns int
-	// Maximum number of idle connections.
-	MaxIdleConns int
 	// Amount of time after which client closes idle connections.
 	// Should be less than server's timeout.
 	// Default is 5 minutes. -1 disables idle timeout check.
-	ConnMaxIdleTime time.Duration
-	// Connection age at which client retires (closes) the connection.
-	// Default is to not close aged connections.
-	ConnMaxLifetime time.Duration
+	IdleTimeout time.Duration
+	// Frequency of idle checks made by idle connections reaper.
+	// Default is 1 minute. -1 disables idle connections reaper,
+	// but idle connections are still discarded by the client
+	// if IdleTimeout is set.
+	IdleCheckFrequency time.Duration
 
 	TLS    bool
 	CAFunc func() *x509.CertPool
@@ -92,19 +95,19 @@ func New(o Options) *redis.Client {
 		Password: o.Password.Value(),
 		DB:       o.DB,
 
-		MaxRetries:      o.MaxRetries,
-		MinRetryBackoff: o.MinRetryBackoff,
-		MaxRetryBackoff: o.MaxRetryBackoff,
-		DialTimeout:     o.DialTimeout,
-		ReadTimeout:     o.ReadTimeout,
-		WriteTimeout:    o.WriteTimeout,
-		PoolFIFO:        o.PoolFIFO,
-		PoolSize:        o.PoolSize,
-		PoolTimeout:     o.PoolTimeout,
-		MinIdleConns:    o.MinIdleConns,
-		MaxIdleConns:    o.MaxIdleConns,
-		ConnMaxIdleTime: o.ConnMaxIdleTime,
-		ConnMaxLifetime: o.ConnMaxLifetime,
+		MaxRetries:         o.MaxRetries,
+		MinRetryBackoff:    o.MinRetryBackoff,
+		MaxRetryBackoff:    o.MaxRetryBackoff,
+		DialTimeout:        o.DialTimeout,
+		ReadTimeout:        o.ReadTimeout,
+		WriteTimeout:       o.WriteTimeout,
+		PoolFIFO:           o.PoolFIFO,
+		PoolSize:           o.PoolSize,
+		MinIdleConns:       o.MinIdleConns,
+		MaxConnAge:         o.MaxConnAge,
+		PoolTimeout:        o.PoolTimeout,
+		IdleTimeout:        o.IdleTimeout,
+		IdleCheckFrequency: o.IdleCheckFrequency,
 	}
 	if o.TLS {
 		var rootCAs *x509.CertPool
--- redis/redis_test.go
+++ redis/redis_test.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/go-redis/redis/v9"
+	"github.com/go-redis/redis/v8"
 	"gotest.tools/v3/assert"
 	"gotest.tools/v3/assert/cmp"
 
