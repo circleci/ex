fa9e52d8ddefa2bd1e328fdc28abe099424758c1
Dan Mullineux <danmux@circleci.com>
2022-11-30 00:58:34 +0000

    Add back the ability to compile a coveraged instrumented binary

--- testing/compiler/doc.go
+++ testing/compiler/doc.go
@@ -1,6 +1,16 @@
 /*
 Package compiler helps efficiently compile and cleanup your services in acceptance tests.
 
-At CircleCI we aim to acceptance test whole compiled binaries.
+This is a small wrapper around the general releases/compiler. The binary created from this package
+will always be stored in a temporary folder.
+
+To instrument a binary with coverage, the main entry point package must include a TestRunMain
+func (or similar )in a test file as per the example in internal/cmd/main_test.go. The work added
+to the compiler must set the WithCoverage flag to true.
+
+For the resultant binary to produce a coverage report it needs to be called with these parameters:
+"-test.run", "^TestRunMain$", "-test.coverprofile", "your_coverage_name.out" and the service needs
+to be able to exit cleanly to flush the report.
+The helper function in testing/runner can make this easier.
 */
 package compiler
--- testing/runner/doc.go
+++ testing/runner/doc.go
@@ -5,5 +5,11 @@ wait for start).
 It is part of our belief that testing binaries that will be shipping into production
 with as little modification as is possible is one of the most effective ways of
 producing high value tests.
+
+Binaries compiled using the testing/compiler can be used to produce binaries with coverage
+instrumentation. In that case setting a CoverageReportPath on a runner for one of those
+binaries will help produce a coverage report. Using runner.Stop will send the running
+process a INT signal, so the service should respond to that elegantly so that the report
+can be flushed to disk.
 */
 package runner
