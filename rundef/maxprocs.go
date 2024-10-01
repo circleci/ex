0d19f4ddff3f0fdbb9c7fbed34eeec9835f19c73
Dominic Lavery <dom@circleci.com>
2024-10-01 19:30:56 +0100

    Correct the limit output of max procs

--- rundef/maxprocs.go
+++ rundef/maxprocs.go
@@ -3,6 +3,7 @@ package rundef
 import (
 	"context"
 	"fmt"
+	"runtime"
 
 	"go.uber.org/automaxprocs/maxprocs"
 
@@ -13,12 +14,17 @@ func MaxProcs(ctx context.Context) (err error) {
 	ctx, span := o11y.StartSpan(ctx, "rundef: max procs")
 	defer o11y.End(span, &err)
 
-	limit, err := maxprocs.Set(maxprocs.Min(1), maxprocs.Logger(func(s string, i ...interface{}) {
-		o11y.Log(ctx, fmt.Sprintf(s, i))
+	_, err = maxprocs.Set(maxprocs.Min(1), maxprocs.Logger(func(s string, i ...interface{}) {
+		// This allows sampling using the rundef namespace which is useful for agents
+		prefixed := fmt.Sprintf("rundef: %s", s)
+
+		o11y.Log(ctx, fmt.Sprintf(prefixed, i))
 	}))
 	if err != nil {
 		return err
 	}
+
+	limit := runtime.GOMAXPROCS(0)
 
 	span.AddField("limit", limit)
 	return err
