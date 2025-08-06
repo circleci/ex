282257304ccbf380994c2e56ec2911f6cef727a7
CircleCI Agent <circleci-agent[bot]@users.noreply.github.com>
2025-08-06 13:54:01 +0000

    Fix flaky test: TestO11y/http_with_token/find_trace

--- o11y/otel/otel_test.go
+++ o11y/otel/otel_test.go
@@ -130,8 +130,18 @@ func TestO11y(t *testing.T) {
 			t.Run("find trace", func(t *testing.T) {
 
 				jc := jaeger.New("http://localhost:16686", tt.cfg.Service)
-				traces, err := jc.Traces(ctx, start)
-				assert.NilError(t, err)
+				var traces []jaeger.Trace
+				poll.WaitOn(t, func(t poll.LogT) poll.Result {
+					var err error
+					traces, err = jc.Traces(ctx, start)
+					if err != nil {
+						return poll.Error(err)
+					}
+					if len(traces) >= 1 {
+						return poll.Success()
+					}
+					return poll.Continue("only got %d traces", len(traces))
+				})
 				assert.Assert(t, cmp.Len(traces, 1))
 
 				spans := traces[0].Spans
